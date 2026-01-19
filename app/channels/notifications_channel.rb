f6f58a9da317ebf435fea597fd9603de06f7fb22
ahntuann <ahntuan284@gmail.com>
2026-01-19 23:48:41 +0700

    Enable asset compile true

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -1,5 +1,6 @@
 class NotificationsChannel < ApplicationCable::Channel
   def subscribed
-    NotificationsChannel.stream_for(current_user)
+    # SỬA LẠI DÒNG NÀY:
+    stream_for current_user
   end
 end
